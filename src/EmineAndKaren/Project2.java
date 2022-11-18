9cd390c99e7defc71633d9c84163a173584eddbf
ksayuriml <sayurimleon@gmail.com>
2022-11-18 13:01:16 -0500

    updates1

--- src/EmineAndKaren/Project2.java
+++ src/EmineAndKaren/Project2.java
@@ -3,8 +3,10 @@ package EmineAndKaren;
 public class Project2 {
 
 	public static void main(String[] args) {
-	
+
 		System.out.println("I want to see you soon..");
+
+		System.out.println("Emine come to Fl.....");
 	}
 
 }
---
+++ src/EmineAndKaren/Project3.java
@@ -0,0 +1,12 @@
+package EmineAndKaren;
+
+public class Project3 {
+
+	public static void main(String[] args) {
+
+		int m = 4;
+		m += 2;
+		System.out.println("Emine you are going to buy your ticket for February" + " " + m + ", 2023");
+	}
+
+}
