b9683ec64b438b39a78e814a2e11a7ddf2ddc53d
ksayuriml <sayurimleon@gmail.com>
2022-11-19 13:34:58 -0500

    Peru

---
+++ src/EmineAndKaren/Project5.java
@@ -0,0 +1,12 @@
+package EmineAndKaren;
+
+public class Project5 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Emine I will go to Peru to celebrate Christmas after 21 years!!!");
+
+		System.out.println("can you imagine????");
+	}
+
+}
