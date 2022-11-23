984ca430a6b09d4afc0967f97e39b1bc24b261d8
ksayuriml <sayurimleon@gmail.com>
2022-11-23 12:01:37 -0500

    New Updates

--- src/EmineAndKaren/Project4.java
+++ src/EmineAndKaren/Project4.java
@@ -3,8 +3,11 @@ package EmineAndKaren;
 public class Project4 {
 
 	public static void main(String[] args) {
-		
-		System.out.println("How fun it will be I can't imagine, your mom will have her Christmas gifts delivered a little early!!");
+
+		System.out.println(
+				"How fun it will be I can't imagine, your mom will have her Christmas gifts delivered a little early!!");
+
+		System.out.println("I know :D");
 	}
 
 }
---
+++ src/EmineAndKaren/Project7EminesParents.java
@@ -0,0 +1,12 @@
+package EmineAndKaren;
+
+public class Project7EminesParents {
+
+	public static void main(String[] args) {
+
+		System.out.println("I AM SO HAPPY YOUR PARENTS ARE COMING");
+
+		// You already know my feelings, but still cannot believe it :D
+	}
+
+}
