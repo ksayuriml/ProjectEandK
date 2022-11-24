f2c1928e58c58c6d562ea0062e6b726ccf84a3f3
E Karakaya <ekarakaya@MacBook-Air.fios-router.home>
2022-11-23 20:58:55 -0500

    commit from E

---
+++ src/EmineAndKaren/Project6.java
@@ -0,0 +1,26 @@
+package EmineAndKaren;
+
+import java.util.ArrayList;
+
+public class Project6 {
+
+	public static void main(String[] args) {
+		
+		ArrayList <String> fam = new ArrayList<>();
+		
+		fam.add("I");
+		fam.add("can't");
+		fam.add("believe");
+		fam.add("they");
+		fam.add("will");
+		fam.add("be");
+		fam.add("really");
+		fam.add("gonna");
+		fam.add("be");
+		fam.add("here");
+		
+		System.out.println(fam);
+
+	}
+
+}
